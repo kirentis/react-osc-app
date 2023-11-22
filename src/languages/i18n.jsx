f82d116bee4c930ec1a17a1b108ec54c610ce89c
kirentis <erik@kirentis.nl>
2023-11-22 18:37:42 +0100

    Your commit message here

--- src/languages/i18n.jsx
+++ src/languages/i18n.jsx
@@ -4,11 +4,13 @@ import { initReactI18next } from "react-i18next";
 
 import nlTranslation from "./nl.json";
 import enTranslation from "./en.json";
+import deTranslation from "./de.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     nl: { translation: nlTranslation },
     en: { translation: enTranslation },
+    de: { translation: deTranslation },
   },
   lng: "nl", // Default language
   fallbackLng: "en", // Fallback language in case a translation is missing
